e7772db75b72acf5c355e0995c9f835eb214969b
Allison MacQueen <allimacq@gmail.com>
2020-08-27 19:20:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,9 @@ def speak_to_grandma(message)
     return "HUH?! SPEAK UP, SONNY!"
   elsif message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  #yelling
+  elsif message == message.upcase()
+    return "NO, NOT SINCE 1938!"
   end
 end
 
